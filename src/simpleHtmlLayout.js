8428217a833e2c257c533836d158e8d544637063
clarkhu <huyujienice@gmail.com>
2022-12-17 16:44:36 +0800

    :sparkles:first init

---
+++ src/simpleHtmlLayout.js
@@ -0,0 +1,32 @@
+const styleAreaReg = /(?<=<style[\s\S]*>)[\s\S]*(?=<\/style>)/g;
+const templateAreaReg = /(?<=<template>)[\s\S]*(?=<\/template>)/g;
+const widthIntStyleReg = /(?<=["'\s])w-\d+(?=["'\s])/g;
+const widthFloatStyleReg = /(?<=["'\s])w-\d+\.\d+(?=["'\s])/g;
+const heightIntStyleReg = /(?<=["'\s])h-\d+(?=["'\s])/g;
+const heightFloatStyleReg = /(?<=["'\s])h-\d+\.\d+(?=["'\s])/g;
+
+function getTemplateArea(str) {
+  const res = str.match(templateAreaReg);
+  return res;
+}
+
+function getStyleArea(str) {
+  const res = str.match(styleAreaReg);
+  return res;
+}
+
+function transformTemplate(str) {
+  return str;
+}
+
+function transformStyle(str) {
+  return str;
+}
+
+function transformVueFile(source) {
+  let res = transformTemplate(source);
+  res = transformStyle(res);
+  return res;
+}
+
+module.exports = transformVueFile;
