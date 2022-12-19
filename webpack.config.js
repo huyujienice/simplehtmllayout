63981921e631386f5e7440d4667d364f71e3384a
clarkhu <huyujienice@gmail.com>
2022-12-19 11:45:58 +0800

    :sparkles:add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
+  mode: "development",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
 };
