e6c82e9a6e1b398ce916bfdcd9ee5cbc6b872aff
clarkhu <huyujienice@gmail.com>
2022-12-21 15:19:40 +0800

    :sparkles:

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,28 @@
 const path = require("path");
 
 module.exports = {
-  entry: './src/core/index.js',
+  entry: "./src/core/index.js",
   mode: "production",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'simplehtmllayout.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "simplehtmllayout.js",
     library: {
-      name: 'simplehtmllayout',
-      type: 'umd',
+      name: "simplehtmllayout",
+      type: "umd",
     },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
   },
 };
