7102de7a97488f3cda9f711484f40c3e0596fcff
clarkhu <huyujienice@gmail.com>
2022-12-22 11:36:31 +0800

    :sparkles:

--- src/core/index.js
+++ src/core/index.js
@@ -2,6 +2,7 @@ import { transformWidthAndHeight } from "./widthAndHeight/index";
 
 export default function transformVueFile(source) {
   const midParams = {};
+  if (!source.includes("simplehtmllayout")) return source;
   let res = transformWidthAndHeight(source, midParams);
   return res;
 }
