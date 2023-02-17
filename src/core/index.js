188f5e588c226f4c91d0b8df49d62318b7e18ac7
clarkhu <huyujienice@gmail.com>
2023-02-17 10:33:25 +0800

    feat: :sparkles:

--- src/core/index.js
+++ src/core/index.js
@@ -23,6 +23,16 @@ export function vitePluginSimplehtmllayout() {
       }
       return { code };
     },
+    configureServer(server) {
+      //todo 中间件处理HMR
+      server.middlewares.use((req, res, next) => {
+        // 自定义请求处理...
+        console.log(`req.method=${req.method}`);
+        console.log(`req.url=${req.url}`);
+        console.log(`req.body=${req.body}`);
+        next();
+      });
+    },
     handleHotUpdate(ctx) {
       //todo
       //热更新中的css模块未处理transfrom过后的源文件
